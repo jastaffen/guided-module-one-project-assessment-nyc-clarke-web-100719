a73aac7f75d2d3ec8b51d59320a7b9765b205891
jastaffen <jack.staffen@gmail.com>
2019-10-21 16:01:03 -0400

    seeds and stuff

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,18 @@
 require_relative '../config/environment'
+require_relative 'methods.rb'
+
+runner
 
 
 
-puts "hello world"
+
+# def add_user
+#     user_input = gets.chomp.strip
+#     user = User.create(name: user_input)
+# end
+
+# puts "Howdy #{add_user.name}! Welcome to Flatiron Simulator 9000"
+
+
+# puts "hello world"
 
